c91f968686b095a6fa1d1a6ef293b5a2170200cd
brendamatias <brendamatias.sobral@gmail.com>
2021-01-08 13:44:28 -0300

    services index.ts test

--- app/Controllers/Http/CompaniesController.ts
+++ app/Controllers/Http/CompaniesController.ts
@@ -1,6 +1,8 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Company from 'App/Models/Company'
 
+import { validateCnpj } from 'App/Services/Utils'
+
 export default class CompaniesController {
   public async index({ request, response }: HttpContextContract) {
     const { page, limit } = request.only(['page', 'limit'])
@@ -11,8 +13,11 @@ export default class CompaniesController {
   }
 
   public async store({ request, response }: HttpContextContract) {
-    const { name, cnpj } = request.only(['category_id'])
+    const { name, cnpj } = request.only(['name', 'cnpj'])
 
+    await validateCnpj(cnpj)
+
+    return
     const company = await Company.create({ name, cnpj })
 
     return response.status(201).send({ company })
---
+++ app/Services/Utils/index.ts
@@ -0,0 +1,24 @@
+export * from 'App/Services/Utils/validateCnpj'
+export * from 'App/Services/Utils/validateCnpj'
+export * from 'App/Services/Utils/validateCnpj'
+export * from 'App/Services/Utils/validateCnpj'
+export * from 'App/Services/Utils/validateCnpj'
+export * from 'App/Services/Utils/validateCnpj'
+export * from 'App/Services/Utils/validateCnpj'
+export * from 'App/Services/Utils/validateCnpj'
+
+// const fs = require('fs')
+
+// const services = {}
+
+// fs.readdirSync(__dirname).forEach((element) => {
+//   if (element !== 'index.ts') {
+//     Object.assign(services, {
+//       [element.slice(0, -3)]: `App/Services/Utils/${element.slice(0, -3)}`,
+//     })
+//   }
+// })
+
+// console.log(services)
+
+// //export  services
